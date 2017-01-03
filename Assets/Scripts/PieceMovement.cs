8d867ad75d784b5b6198c402cae06fd35db530de
Ahmed <ahmedgouda9484@gmail.com>
2017-01-03 16:35:07 -0600

    Trying to figure out selecting gameobjects by mouse click....

--- Assets/Scripts/PieceMovement.cs
+++ Assets/Scripts/PieceMovement.cs
@@ -12,11 +12,28 @@ public class PieceMovement : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+		boxCollider = GetComponent<BoxCollider2D> ();
+		rigidBody = GetComponent<Rigidbody2D> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+//		if(Input.GetMouseButtonDown(0))
+//		{
+//			Debug.Log(Input.mousePosition);
+//		}
+		if ( Input.GetMouseButtonDown (0)){ 
+//			RaycastHit2D hit; 
+//			Vector2 start = Input.mousePosition;
+//			Vector2 end = Input.mousePosition;
+//			hit = Physics2D.Raycast (start, , GameLayer);
+
+			Vector2 rayPos = new Vector2 (Camera.main.ScreenToWorldPoint (Input.mousePosition).x, Camera.main.ScreenToWorldPoint (Input.mousePosition).y);
+			RaycastHit2D hit = Physics2D.Raycast (rayPos, Vector2.zero, 0f);
+
+			if(hit){
+				Debug.Log (hit.transform.name);
+			}
+		}
 	}
 }
