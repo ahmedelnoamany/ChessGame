14afbae87e257ca0f092587606b9e248dfc2e4f2
Ahmed <ahmedgouda9484@gmail.com>
2017-01-05 21:56:10 -0600

    Added piece selection logic. White turn = white pieces can be selcted only and vice versa.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,11 +7,15 @@ public class GameManager : MonoBehaviour {
 	public static GameManager instance = null;		//instance of GameManager.
 	public BoardManager boardScript;		//to store our boardManager script.
 
-	[HideInInspector]
-	public bool whoseTurn = true;	//white turn = true. Black turn = false.
+	public bool whoseTurn;	//white turn = true. Black turn = false.
 
 	private bool settingUp;		//true if setting up the game. False otherwise.
-
+	private BoxCollider2D boxCollider;
+	private Rigidbody2D rigidBody;
+	private Renderer spriteRenderer;
+	private GameObject selected;
+	private GameObject secondSelected;
+	private bool alreadySelected = false;	//if a piece has already been selected
 
 
 
@@ -26,6 +30,44 @@ public class GameManager : MonoBehaviour {
 		DontDestroyOnLoad (gameObject);
 		boardScript = GetComponent<BoardManager> ();	//Getting reference to BoardManager
 		InitGame ();
+	}
+	void Update () {
+		if ( Input.GetMouseButtonDown (0)){ 
+			Vector2 rayPos = new Vector2 (Camera.main.ScreenToWorldPoint (Input.mousePosition).x, Camera.main.ScreenToWorldPoint (Input.mousePosition).y);
+			RaycastHit2D hit = Physics2D.Raycast (rayPos, Vector2.zero, 0f);
+			if(hit){
+				Debug.Log (hit.transform.name);
+				Debug.Log (hit.transform.position);
+				Selection (hit);
+			}
+		}
+	}
+
+	//Piece selected Logic
+	void Selection(RaycastHit2D hit){
+
+		if (alreadySelected) {	//if another piece has been selected, unhighlight the first.
+			spriteRenderer.material.color = Color.white;
+		}
+		selected = hit.collider.gameObject;
+		spriteRenderer = selected.GetComponent<Renderer> ();
+		if (WhosTurn () == true && hit.transform.tag == "WhiteTeam") {	//whites turn. check piece tag only allow white to be selected.
+			HighlightSelection (selected);	
+		} 
+		if(WhosTurn() == false && hit.transform.tag == "BlackTeam"){
+				HighlightSelection (selected);
+		}
+	}
+	//Highlight selected piece	
+	void HighlightSelection(GameObject go){
+		spriteRenderer.material.color = Color.blue;
+		alreadySelected = true;
+		}
+
+	bool WhosTurn(){
+		if (whoseTurn == true)
+			return true;	//white
+		return false;	//black
 	}
 
 	//Called by Awake(). Initializes the game by the GameManager.
--- Assets/Scripts/PieceMovement.cs
+++ Assets/Scripts/PieceMovement.cs
@@ -6,34 +6,54 @@ public class PieceMovement : MonoBehaviour {
 
 	public float moveTime = 0.1f;	//how long it takes to move a piece.
 	public LayerMask GameLayer;	//collisions happen here.
+	public LayerMask BoardLayer;
 
 	private BoxCollider2D boxCollider;
 	private Rigidbody2D rigidBody;
+	private Renderer spriteRenderer;
+	private GameObject selected;
+	private GameObject secondSelected;
+	private bool alreadySelected = false;
 
 	// Use this for initialization
 	void Start () {
-		boxCollider = GetComponent<BoxCollider2D> ();
-		rigidBody = GetComponent<Rigidbody2D> ();
+		//boxCollider = GetComponent<BoxCollider2D> ();
+		//rigidBody = GetComponent<Rigidbody2D> ();
+		//spriteRenderer = GetComponent<Renderer> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-//		if(Input.GetMouseButtonDown(0))
-//		{
-//			Debug.Log(Input.mousePosition);
-//		}
 		if ( Input.GetMouseButtonDown (0)){ 
-//			RaycastHit2D hit; 
-//			Vector2 start = Input.mousePosition;
-//			Vector2 end = Input.mousePosition;
-//			hit = Physics2D.Raycast (start, , GameLayer);
-
 			Vector2 rayPos = new Vector2 (Camera.main.ScreenToWorldPoint (Input.mousePosition).x, Camera.main.ScreenToWorldPoint (Input.mousePosition).y);
 			RaycastHit2D hit = Physics2D.Raycast (rayPos, Vector2.zero, 0f);
-
 			if(hit){
 				Debug.Log (hit.transform.name);
+				Debug.Log (hit.transform.position);
+				if (!alreadySelected) {
+					selected = hit.collider.gameObject;
+					Test (selected);
+				}
+				else{
+					secondSelected = hit.collider.gameObject;
+					Test (secondSelected);
+				}
 			}
+		}
+	}
+
+	void Test(GameObject go){
+		
+		spriteRenderer = go.GetComponent<Renderer> ();
+		if (!alreadySelected) {
+			spriteRenderer.material.color = Color.blue;
+			alreadySelected = true;
+		}
+		else{
+			
+			
+			
+			
 		}
 	}
 }
